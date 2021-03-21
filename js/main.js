3675db88fc1833c71fdb17277ea0b722b4cf90f1
fukadashigeru <sakusaku721@gmail.com>
2021-03-21 14:26:58 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+'use strict';
+
+{
+  class Sum {
+    constructor() {
+    }
+
+    calculate() {
+      const p = document.createElement('p');
+      const a = document.querySelector('#inputA');
+      const b = document.querySelector('#inputB');
+      const sum = document.querySelector('#sum');
+      const valueA = Number(a.value);
+      const valueB = Number(b.value);
+
+      p.textContent = `合計：${valueA + valueB}`;
+
+      while (sum.firstChild) {
+        sum.removeChild(sum.firstChild);
+      }
+      sum.appendChild(p);
+    }
+  }
+  const sum = new Sum();
+  sum.calculate();
+
+  const inputs = document.querySelectorAll('input')
+  inputs.forEach((input) => {
+    input.addEventListener('input', () => {
+      sum.calculate();
+    })
+  })
+}
